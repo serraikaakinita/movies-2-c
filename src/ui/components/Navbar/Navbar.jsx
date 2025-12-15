67df2256d0a849e115436eb7283125dc71a39831
erieta04 <erietawraiopoulou@gmail.com>
2025-12-15 16:49:23 +0200

    Update Navbar.jsx

--- src/ui/components/Navbar/Navbar.jsx
+++ src/ui/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
       <div className="navbar_links">
         <Link to="/home">Home</Link>
-        <Link to="/quizzes">Quizzes</Link> {/* 🔥 ΑΥΤΟ ΕΔΩ */}
+        <Link to="/quizzes">Quizzes</Link> 
 
         {isUserLoggedIn ? (
           <Row gap="0.5rem">
